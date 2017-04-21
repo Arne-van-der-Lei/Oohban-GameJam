32d1849abab4f04003b01e5f438cc3de13933524
houdhakker2 <arne@vanderlei.be>
2017-04-21 23:40:02 +0200

    cration of the world and base block class

---
+++ OohbanGameJam/Assets/Code/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    bool[] InputSides = new bool[4] { false, false, false, false };
+    bool[] OutputSides = new bool[4] { false, false, false, false };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ OohbanGameJam/Assets/Code/World.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : Singleton<World> {
+
+    GameObject[,,] block;
+    public Vector3 size = new Vector3(5, 5, 5);
+    public Vector3 offset = new Vector3(1, 1, 1);
+    public GameObject[] PrefabList;
+
+	// Use this for initialization
+	void Start () {
+        block = new GameObject[(int)size.x, (int)size.y, (int)size.z];
+		for(int i = 0; i < size.x; i++)
+        {
+            for(int j = 0; j < size.y; j++)
+            {
+                for (int k = 0; k < size.z; k++)
+                {
+                    block[i, j, k] = Instantiate(PrefabList[0], new Vector3(offset.x * i, offset.y * j, offset.z * k), Quaternion.identity);
+                    block[i, j, k].transform.parent = transform;
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
