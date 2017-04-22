16cd7cce87cfa0e77e925dc16396e7c81815db97
houdhakker2 <arne@vanderlei.be>
2017-04-22 16:02:48 +0200

    added code to handel changes in water

---
+++ OohbanGameJam/Assets/Code/WaterChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterChanger : MonoBehaviour {
+
+    public Mesh mesh25;
+    public Mesh mesh50;
+    public Mesh mesh75;
+    public Mesh mesh100;
+
+    public MeshFilter MeshFiter;
+    // Use this for initialization
+    void Start () {
+        MeshFiter = GetComponent<MeshFilter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeMesh(float value)
+    {
+        if (value >= 1.00f)
+        {
+            MeshFiter.mesh = mesh100;
+        }
+        else if (value >= 0.75f)
+        {
+            MeshFiter.mesh = mesh75;
+        }
+        else if (value >= 0.50f)
+        {
+            MeshFiter.mesh = mesh50;
+        }
+        else if (value >= 0.25f)
+        {
+            MeshFiter.mesh = mesh25;
+        }
+    }
+}
