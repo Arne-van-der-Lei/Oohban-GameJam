a62fbef90b8b707cc7c9f551cf199a7dc93b4b55
houdhakker2 <arne@vanderlei.be>
2017-04-24 02:11:58 +0200

    almost finished product

---
+++ OohbanGameJam/Assets/Code/WaterGate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGate : WaterChanger {
+
+    public bool Ison = false;
+
+    private void Update()
+    {
+    }
+
+    public override void ChangeMesh(float value, Block.sides side)
+    {
+        if(Ison)
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 90);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 0);
+        }
+
+        if (value >= 1.00f)
+        {
+            MeshFiter.mesh = mesh100;
+            transform.Rotate(0, 0, 180);
+        }
+        else if (value >= 0.75f)
+        {
+            MeshFiter.mesh = mesh75;
+            transform.Rotate(0, 0, 180);
+        }
+        else if (value >= 0.50f)
+        {
+            MeshFiter.mesh = mesh50;
+        }
+        else if (value >= 0.25f)
+        {
+            MeshFiter.mesh = mesh25;
+            transform.Rotate(-90, 0, 0);
+        }
+    }
+}
---
+++ OohbanGameJam/Assets/Code/WaterWeel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWeel : LeverSwitcher {
+
+    
+    private void Update()
+    {
+        if (World.Instance.GetBlock((int)transform.position.x,(int)transform.position.y-1,(int)transform.position.z).WaterAmount >= 0.5)
+        {
+            gate.IsOn = true;
+        }
+        else
+        {
+            gate.IsOn = false;
+        }
+    }
+}
